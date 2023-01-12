8721b507fdda3f87b16840da53b96e890084775a
thomas <thomas@emotion.nl>
2023-01-12 22:23:32 +0100

    Production ready now, downgraded some shit

--- next.config.js
+++ next.config.js
@@ -81,16 +81,6 @@ module.exports = nextTranslate(
         use: ['raw-loader', 'glslify-loader'],
       })
 
-      if (!dev && !isServer) {
-        // Replace React with Preact only in client production build
-        Object.assign(config.resolve.alias, {
-          'react/jsx-runtime.js': 'preact/compat/jsx-runtime',
-          react: 'preact/compat',
-          'react-dom/test-utils': 'preact/test-utils',
-          'react-dom': 'preact/compat',
-        })
-      }
-
       return config
     },
   })
