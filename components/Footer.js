1242b4946cc7468341fbee8b3b4a4c397f2e938d
thomas <thomas@emotion.nl>
2023-02-15 10:17:35 +0100

    Show social icons

--- components/Footer.js
+++ components/Footer.js
@@ -8,12 +8,12 @@ export default function Footer() {
   const size = 6
   return (
     <footer>
+      <div className="mb-3 flex justify-center space-x-4">
+        <SocialIcon kind="github" href={siteMetadata.github} size="5" />
+        <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="5" />
+        <SocialIcon kind="twitter" href={siteMetadata.twitter} size="5" />
+      </div>
       <div className={`mt-16 mb-4 flex flex-col `}>
-        <div className="mb-3 flex justify-center space-x-4">
-          <SocialIcon kind="github" href={siteMetadata.github} size="6" />
-          <SocialIcon kind="linkedin" href={siteMetadata.linkedin} size="6" />
-          <SocialIcon kind="twitter" href={siteMetadata.twitter} size="6" />
-        </div>
         <div className="mb-6 flex justify-center space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{`© ${new Date().getFullYear()}`}</div>
           <div>{` • `}</div>
